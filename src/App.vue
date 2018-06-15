17d00ca28494910ae03fa4a5675e9339034989ba
lukasavovic <dajana.m.miljanovic@gmail.com>
2018-06-15 12:57:23 +0200

    just name change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-link to="/">Homepage</router-link>
+    <router-link to="/">Homepag</router-link>
   </div>
 </template>
 
