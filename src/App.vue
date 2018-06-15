850bb748ae77a5c68b9c8330c439d4fb629e6cd9
lukasavovic <luka.savovic199323@gmail.com>
2018-06-15 13:01:34 +0200

    name change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-link to="/">Homepag</router-link>
+    <router-link to="/">Homepage</router-link>
   </div>
 </template>
 
