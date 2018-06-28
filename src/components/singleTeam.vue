eb9e4a9be99267a922b986ddf226b84c27bb3c39
Luka Savovic <luka.savovic199323@gmail.com>
2018-06-28 07:26:11 +0200

    css team changes

--- src/components/singleTeam.vue
+++ src/components/singleTeam.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="team in teams" :key="team.id" v-if="team.id == $route.params.id">
-             <h2 v-text="team.name"></h2>
+             <h1 v-text="team.name"></h1>
              <img :src="team.flag" alt="">
              <router-link :to="'/singleGroup/' + team.group">Group: {{team.group}}</router-link>
              <h2> Fifa ranking: {{team.fifaRanking}}</h2>
@@ -31,6 +31,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
+div{
+    display: grid;
+    grid-template-columns: 1fr;
+    a{
+        font-size: 25px;
+    }
+    h2{
+        font-weight: normal;
+    }
+}
     img{
         display: block;
         margin: 0 auto;
